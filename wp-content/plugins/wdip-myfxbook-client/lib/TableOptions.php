a900403d9b68e45023583a4bbbdf7fadd8fb7242
Igor Popravka <igor.popravka@dukascopy.com>
2017-11-02 12:09:38 +0200

    MYFXBOOK-19

--- wp-content/plugins/wdip-myfxbook-client/lib/TableOptions.php
+++ wp-content/plugins/wdip-myfxbook-client/lib/TableOptions.php
@@ -7,9 +7,14 @@ namespace WDIP\Plugin;
  * Time: 13:18
  *
  * @property $tableData
+ * @property $defaultcells
  */
 class TableOptions extends MyFXBookOptions {
+    const NOT_AVAILABLE = 'N/A';
+
     protected function generate() {
+        $this->generateDefaultCells();
+
         $data = [];
         $total_compounded = 0;
         foreach ($this->accountid as $id) {
@@ -20,9 +25,8 @@ class TableOptions extends MyFXBookOptions {
         $body = [];
         foreach ($data as $dt) {
             $combine_data = array_combine($dt[0], $dt[1]);
-            $year = 'N/A';
-            $months = ['JAN' => 'N/A', 'FEB' => 'N/A', 'MAR' => 'N/A', 'APR' => 'N/A', 'MAY' => 'N/A', 'JUN' => 'N/A', 'JUL' => 'N/A', 'AUG' => 'N/A', 'SEP' => 'N/A', 'OCT' => 'N/A', 'NOV' => 'N/A', 'DEC' => 'N/A'];
-            $total = 0;
+            list($year, $months, $total) = $this->getDefaultTableRow();
+
             foreach ($combine_data as $date => $val) {
                 if (preg_match('/^([a-z]{3})\s(\d{4})$/i', $date, $match) > 0) {
                     $year = $match[2];
@@ -32,12 +36,14 @@ class TableOptions extends MyFXBookOptions {
                 }
             }
 
+            $months = $this->mergeDefaultCells($year, $months);
+
             if (isset($body[$year])) {
                 foreach ($months as $m => $v) {
-                    if ($body[$year]['MONTHS'][$m] != 'N/A' && $v != 'N/A') {
+                    if ($body[$year]['MONTHS'][$m] != self::NOT_AVAILABLE && $v != self::NOT_AVAILABLE) {
                         $body[$year]['MONTHS'][$m] += $v;
                         $body[$year]['TOT'] += $v;
-                    } else if ($body[$year]['MONTHS'][$m] == 'N/A' && $v != 'N/A') {
+                    } else if ($body[$year]['MONTHS'][$m] == self::NOT_AVAILABLE && $v != self::NOT_AVAILABLE) {
                         $body[$year]['MONTHS'][$m] = $v;
                         $body[$year]['TOT'] += $v;
                     }
@@ -58,7 +64,7 @@ class TableOptions extends MyFXBookOptions {
     }
 
     public function getTickClass($value) {
-        if($value !== 'N/A'){
+        if ($value !== self::NOT_AVAILABLE) {
             $num = floatval($value);
             return $num >= 0 ? 'table-tick-green' : 'table-tick-red';
         }
@@ -68,7 +74,7 @@ class TableOptions extends MyFXBookOptions {
     public function format($value) {
         if ($value === 0) {
             return '0.00%';
-        } else if ($value !== 'N/A') {
+        } else if ($value !== self::NOT_AVAILABLE) {
             return number_format($value, 2) . "%";
         }
         return $value;
@@ -77,5 +83,58 @@ class TableOptions extends MyFXBookOptions {
     public function formatMonthValue(array $data, $month) {
         $value = $data['MONTHS'][$month];
         return $this->format($value);
+    }
+
+    private function getDefaultTableRow() {
+        return [
+            self::NOT_AVAILABLE,
+            [
+                'JAN' => self::NOT_AVAILABLE,
+                'FEB' => self::NOT_AVAILABLE,
+                'MAR' => self::NOT_AVAILABLE,
+                'APR' => self::NOT_AVAILABLE,
+                'MAY' => self::NOT_AVAILABLE,
+                'JUN' => self::NOT_AVAILABLE,
+                'JUL' => self::NOT_AVAILABLE,
+                'AUG' => self::NOT_AVAILABLE,
+                'SEP' => self::NOT_AVAILABLE,
+                'OCT' => self::NOT_AVAILABLE,
+                'NOV' => self::NOT_AVAILABLE,
+                'DEC' => self::NOT_AVAILABLE
+            ],
+            0
+        ];
+    }
+
+    private function generateDefaultCells() {
+        if (!empty($this->defaultcells)) {
+            $default = explode(',', $this->defaultcells);
+            $formatted = [];
+            foreach ($default as $cell) {
+                if (preg_match('/^\s*([a-z]{3})\s*:\s*(\d{4})\s*=\s*([0-9.]+)\s*$/i', $cell, $match)) {
+                    if (!isset($formatted[$match[2]])) {
+                        $formatted[$match[2]] = [];
+                    }
+                    $formatted[$match[2]][strtoupper($match[1])] = $match[3];
+                }
+            }
+            $this->defaultcells = $formatted;
+        }
+    }
+
+    private function mergeDefaultCells($year, array $months) {
+        if (!empty($this->defaultcells)) {
+            foreach ($this->defaultcells as $y => $m) {
+                if ($year == $y) {
+                    foreach ($m as $n => $v) {
+                        if (isset($months[$n]) && $months[$n] == self::NOT_AVAILABLE) {
+                            $months[$n] = $v;
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        return $months;
     }
 }

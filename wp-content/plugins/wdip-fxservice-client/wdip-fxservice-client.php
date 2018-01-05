27d360d36fac555ae5d7aa73bdba0edc9333f1e4
Igor Popravka <igor.popravka@dukascopy.com>
2018-01-05 11:28:50 +0200

    FXBLUE-B#19-1

--- wp-content/plugins/wdip-fxservice-client/wdip-fxservice-client.php
+++ wp-content/plugins/wdip-fxservice-client/wdip-fxservice-client.php
@@ -2,7 +2,7 @@
 /**
  * @Plugin Name: FX-Service Client
  * @Description: Builds Charts/Graphs/Tables using the data from API of the FX-Service Clients: [<a href="https://www.myfxbook.com/api">MYFXBook</a>] [<a href="https://www.fxblue.com">FXBlue Client</a>]
- * @Version:     2.2.9
+ * @Version:     2.2.10
  * @Author:      Igor Popravka
  * @Author URI:  https://www.upwork.com/freelancers/~010854a54a1811f970
  */
